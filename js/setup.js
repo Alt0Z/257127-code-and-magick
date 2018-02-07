0eae530d33027d39b2b8f6c74a7d01df18d96502
Roman Shakhanov <altair0908@gmail.com>
2018-02-07 22:31:30 +0700

    Final edits

--- js/setup.js
+++ js/setup.js
@@ -1,43 +1,34 @@
 'use strict';
 
+var WIZARDS_INDEX = 3;
+
 var setupWindow = document.querySelector('.setup');
 
-var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-var WIZARDS_INDEX = 3;
+var getRandomIndex = function () {
+  return Math.floor(Math.random() * WIZARDS_INDEX);
+};
+
+var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'].sort(getRandomIndex);
+var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'].sort(getRandomIndex);
+var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'].sort(getRandomIndex);
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 var wizards = [];
 var similarListElement = setupWindow.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
-var getRandomIndex = function () {
-  return Math.floor(Math.random() * NAMES.length);
-};
-
-var getRandomArr = function (array) {
-  var randomArr = array;
-  return randomArr.sort(getRandomIndex);
-};
-
 var createWizard = function (name, lastName, coatColor, eyesColor) {
-  var wizard = {
+  return {
     name: name,
     lastName: lastName,
     coatColor: coatColor,
     eyesColor: eyesColor
   };
-
-  return wizard;
 };
 
 var generateWizardsArray = function () {
-  var randomNames = getRandomArr(NAMES);
-  var randomLastNames = getRandomArr(LAST_NAMES);
-  var randomCoatColors = getRandomArr(COAT_COLORS);
-  var randomEyesColors = getRandomArr(EYES_COLORS);
+
   for (var i = 0; i <= WIZARDS_INDEX; i++) {
-    wizards[i] = createWizard(randomNames[i], randomLastNames[i], randomCoatColors[i], randomEyesColors[i]);
+    wizards[i] = createWizard(names[i], surnames[i], coatColors[i], eyesColors[i]);
   }
 };
 

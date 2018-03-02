a6e2193fe2d6208b5149e2c1f79fb72ab292029b
Roman Shakhanov <altair0908@gmail.com>
2018-03-02 16:00:16 +0700

    small fixes

--- js/setup.js
+++ js/setup.js
@@ -13,7 +13,7 @@
 
   window.names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'].sort(window.getRandomIndex);
   window.surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'].sort(window.getRandomIndex);
-  window.coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'].sort(getRandomIndex);
+  window.coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'].sort(window.getRandomIndex);
   window.eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
   window.fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
   window.wizards = [];
